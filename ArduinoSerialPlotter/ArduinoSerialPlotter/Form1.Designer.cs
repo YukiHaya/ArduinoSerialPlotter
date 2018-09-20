73c3d3f7b950795b8156e54d05d328b08047b484
yuki <csbrhaya_1902@yahoo.ne.jp>
2018-09-20 23:40:08 +0900

    [Add]新規ファイル
    新規ファイルを追加

---
+++ ArduinoSerialPlotter/ArduinoSerialPlotter/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace ArduinoSerialPlotter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.IsMarginVisible = false;
+            chartArea1.AxisX.MajorGrid.Enabled = false;
+            chartArea1.AxisX.ScaleView.Zoomable = false;
+            chartArea1.AxisX.ScrollBar.Enabled = false;
+            chartArea1.AxisX.Title = "経過時間(s)";
+            chartArea1.AxisY.MajorGrid.Enabled = false;
+            chartArea1.AxisY.Title = "気温(℃)";
+            chartArea1.CursorX.IsUserEnabled = true;
+            chartArea1.CursorY.IsUserEnabled = true;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(172, 30);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "気温";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1000, 600);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(38, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "接続";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(38, 121);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "切断";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 213);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 417);
+            this.textBox1.TabIndex = 3;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM5";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(26, 30);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Text = "COMポート";
+            this.comboBox1.DropDown += new System.EventHandler(this.comboBox1_DropDown);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "300",
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "14400",
+            "19200",
+            "28800",
+            "38400",
+            "57600",
+            "115200"});
+            this.comboBox2.Location = new System.Drawing.Point(26, 56);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 20);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.Text = "転送速度";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(38, 184);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "クリア";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 661);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "ArduinoSerialPlotter";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ArduinoSerialPlotter/ArduinoSerialPlotter/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace ArduinoSerialPlotter
+{
+    public partial class Form1 : Form
+    {
+        string RxString;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            RxString = serialPort1.ReadLine(); //シリアル通信の入力
+            this.Invoke(new EventHandler(DisplayText));
+            this.Invoke(new EventHandler(showChart));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == true) //すでにポートが開かれている場合
+            {
+                MessageBox.Show("COMポートを閉じてください", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (comboBox1.Text != "COMポート" && comboBox2.Text != "転送速度") //comboBox1とcomboBox2に設定があればシリアルポートを開く
+            {
+                serialPort1.PortName = comboBox1.Text; //comboBox1.Textをシリアルポートのポート名として設定
+
+                serialPort1.BaudRate = int.Parse(comboBox2.Text);
+
+                if (serialPort1.IsOpen == true) //シリアルポートが使われていたら一旦閉じる
+                {
+                    serialPort1.Close();
+                }
+
+                if (serialPort1.IsOpen == false) //シリアルポートが使われていないのを確認して開く
+                {
+                    serialPort1.Open();
+                }
+
+                if (serialPort1.IsOpen == true)
+                {
+                    MessageBox.Show("接続成功\n" + serialPort1.PortName.ToString(), "成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("COMポートエラー", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else if (comboBox1.Text == "COMポート" && comboBox2.Text != "転送速度") //COMポートが選択されていない場合
+            {
+                MessageBox.Show("COMポートを選択してください", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (comboBox1.Text != "COMポート" && comboBox2.Text == "転送速度") //転送速度が選択されていない場合
+            {
+                MessageBox.Show("転送速度を選択してください", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else //どちらも選択されていない場合
+            {
+                MessageBox.Show("COMポートと転送速度を選択してください", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close(); //切断
+            comboBox1.Text = "COMポート";
+            comboBox2.Text = "転送速度";
+            MessageBox.Show("切断されました", "切断", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void DisplayText(object sender, EventArgs e)
+        {
+            textBox1.AppendText(RxString + Environment.NewLine); //テキストボックスに出力
+        }
+
+        private void showChart(object sender, EventArgs e)
+        {
+            if (RxString.Contains(",")) //","が含まれていない場合に例外がスローされるのを防ぐ
+            {
+                string[] strArrayData = RxString.Split(',');
+                int x = int.Parse(strArrayData[0]);
+                int y = int.Parse(strArrayData[1]);
+
+                chart1.Series["気温"].Points.AddXY(x, y);
+
+                chart1.ChartAreas[0].AxisX.Maximum = 3600;
+                chart1.ChartAreas[0].AxisX.Minimum = 0;
+                if (x > 3600)
+                {
+                    chart1.ChartAreas[0].AxisX.Maximum = x;
+                    chart1.ChartAreas[0].AxisX.Minimum = x - 3600;
+                }
+
+                chart1.ChartAreas[0].AxisY.Maximum = 40;
+                chart1.ChartAreas[0].AxisY.Minimum = 10;
+            }
+        }
+
+        private void comboBox1_DropDown(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+
+            comboBox1.Items.Clear();
+
+            foreach (string port in ports)
+            {
+                comboBox1.Items.Add(port);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+        }
+    }
+}
